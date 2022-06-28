3b4c0a288a54d14f2e6b85417c0bf51d648dee60
L-Karimi <luciahkei101@gmail.com>
2022-06-28 08:27:31 +0300

    added conditionals

---
+++ 04_Day_Conditionals/conditionals.js
@@ -0,0 +1,23 @@
+let isRaining=true;
+if(isRaining){
+    console.log("Remember to take your rainy coat")
+}else{
+    console.log("Its gonna be cold")
+}
+
+let num =6;
+if (num==6){
+    console.log("This is my lucky number")
+}
+
+// if else if else
+let weather = 'sunny'
+if (weather === 'rainy') {
+  console.log('You need a rain coat.')
+} else if (weather === 'cloudy') {
+  console.log('It might be cold, you need a jacket.')
+} else if (weather === 'sunny') {
+  console.log('Go out freely.')
+} else {
+  console.log('No need for rain coat.')
+}
